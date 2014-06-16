bf87ec79d23981129ea5a5726f81a33b61e9d1fe
Esteban Hebel <ehebel@gmail.com>
2014-06-15 21:26:21 -0400

    Creacion de Libro electrónico de Casos

--- repocasos/autocomplete_light_registry.py
+++ repocasos/autocomplete_light_registry.py
@@ -4,6 +4,7 @@ from snomedct.models import sct_description
 
 autocomplete_light.register(sct_description,
     search_fields=['term'],
+    choices=sct_description.objects.filter(descriptiontype__exact=0),
     attrs={
         'placeholder': 'SNOMED-CT (BETA)',
         'data-widget-minimum-characters': 1,
