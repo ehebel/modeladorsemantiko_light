9b50a9c8fde9250e55d58696b072d87bd9765c36
Esteban Hebel <ehebel@gmail.com>
2014-06-16 21:36:25 -0400

    Creacion de Libro electrónico de Casos

--- repocasos/models.py
+++ repocasos/models.py
@@ -10,15 +10,17 @@ OPCIONES_INSTITUCION = (
     )
 
 OPCIONES_DOMINIO = (
-    ('1','CUERPO'),
+    ('1','Cuerpo'),
     ('2','Mama'),
     ('3','MSK'),
+    ('4','Pediatria'),
+    ('5','Neuro'),
     ('4','Otro'),
     )
 
 class BaseModel(models.Model):
     fecha_creacion = models.DateTimeField(null=False, auto_now_add=True)
-    usuario_creador = models.ForeignKey(User, null=False, blank=False, editable=False, related_name='%(app_label)s_%(class)s_related_crea')
+    usuario_creador = models.ForeignKey(User, null=True, blank=False, editable=False, related_name='%(app_label)s_%(class)s_related_crea')
     fecha_ult_mod = models.DateTimeField(null=True, auto_now=True)
     usuario_ult_mod = models.ForeignKey(User, null=True, blank=False, editable=False, related_name='%(app_label)s_%(class)s_related_modif')
 
@@ -34,13 +36,14 @@ class radlex(models.Model):
         return unicode(self.name)
 
 class caso(BaseModel):
-    fechacaso = models.DateField('Fecha (dd/mm/aaaa)')
+    fechacaso = models.DateField('Fecha')
     nombre = models.CharField('Nombre',max_length=255)
     appaterno = models.CharField('Apellido Paterno',max_length=255)
-    apmaterno = models.CharField('Apellido Materno',max_length=255)
+    apmaterno = models.CharField('Apellido Materno',max_length=255, blank=True, null=True)
     institucion = models.CharField('Institucion',max_length=5, choices=OPCIONES_INSTITUCION)
     observacion = models.CharField('Observacion',max_length=255)
     dominio = models.CharField(choices=OPCIONES_DOMINIO,max_length=255)
+    seguimiento = models.BooleanField(u'Requiere seguimiento?')
     tagging = models.ManyToManyField(sct_description, blank=True)
     url = models.URLField(blank=True)
 
